fdead5afbe28b41e6a7b90770039c43d8d99f4f5
Lev Lebedev <levask00@gmail.com>
2020-11-10 13:12:07 +0500

    removed old form

--- login.php
+++ login.php
@@ -1,20 +1,23 @@
 <? require "navbar.php" ?>
 <div class="container">
-    <form action="/php/login.php" method="post">
-        <input type="text" name="login" placeholder="Логин"> <br><br>
-        <input type="password" name="password" placeholder="Пароль"> <br><br>
-        <input type="submit" name="go">
-    </form>
-    <form action="/php/login.php">
-        <div >
-            <label for="login">Логин</label>
-            <input type="text" class="form-control" id="login" aria-describedby="login" name="login">
-        </div>
-        <div class="form-group">
-            <label for="password">Пароль</label>
-            <input type="password" class="form-control" id="password" name="password">
-        </div>
-        <button type="submit" class="btn btn-primary btn-block">Войти</button>
-    </form>
+    <div class="col-md-3">
+        
+    </div>
+    <div class="col-md-6">
+        <form action="/php/login.php">
+            <div >
+                <label for="login">Логин</label>
+                <input type="text" class="form-control" id="login" aria-describedby="login" name="login">
+            </div>
+            <div class="form-group">
+                <label for="password">Пароль</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">Войти</button>
+        </form>
+    </div>
+    <div class="col-md-3">
+        
+    </div>
 </div>
 <? require "footer.php" ?>
