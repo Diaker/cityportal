a8264f8bfd9a91eb452465cdfc9af44dbfc5be03
Lev Lebedev <levask00@gmail.com>
2020-11-10 13:03:51 +0500

    added footer

--- index.php
+++ index.php
@@ -20,45 +20,38 @@
     <center>
         <h2>Последние решенные проблемы</h2>
         <br>
-<div class="row">
-      <div class="col-sm-6 col-md-4">
-           <div class="img-comp-container">
-              <div class="img-comp-img">
-                    <img src="img/problem1.jpeg" width="300" height="300">
-              </div>   
-              <div class="img-comp-img img-comp-overlay">
-                    <img src="img/decision1.jpeg" width="300" height="300">
-              </div>
-            </div>
-        </div>
-         <div class="col-sm-6 col-md-4">
-            <div class="img-comp-container">
-              <div class="img-comp-img">
-                    <img src="img/problem3.jpeg" width="300" height="300">
-              </div>   
-              <div class="img-comp-img img-comp-overlay">
-                    <img src="img/decision3.jpeg" width="300" height="300">
-              </div>
-            </div>
+        <div class="row">
+              <div class="col-sm-6 col-md-4">
+                   <div class="img-comp-container">
+                      <div class="img-comp-img">
+                            <img src="img/problem1.jpeg" width="300" height="300">
+                      </div>   
+                      <div class="img-comp-img img-comp-overlay">
+                            <img src="img/decision1.jpeg" width="300" height="300">
+                      </div>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-md-4">
+                    <div class="img-comp-container">
+                      <div class="img-comp-img">
+                            <img src="img/problem3.jpeg" width="300" height="300">
+                      </div>   
+                      <div class="img-comp-img img-comp-overlay">
+                            <img src="img/decision3.jpeg" width="300" height="300">
+                      </div>
+                    </div>
+                 </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="img-comp-container">
+                      <div class="img-comp-img">
+                            <img src="img/problem4.jpeg" width="300" height="300">
+                      </div>   
+                      <div class="img-comp-img img-comp-overlay">
+                            <img src="img/decision4.jpeg" width="300" height="300">
+                      </div>
+                    </div>
+                </div>
          </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="img-comp-container">
-              <div class="img-comp-img">
-                    <img src="img/problem4.jpeg" width="300" height="300">
-              </div>   
-              <div class="img-comp-img img-comp-overlay">
-                    <img src="img/decision4.jpeg" width="300" height="300">
-              </div>
-            </div>
-        </div>
- </div>
     </center>
 </div>
-<script src="js/slider.js"></script>
-<script >
-    initComparisons();
-</script>
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="js/bootstrap.js"></script>
-</body>
-</html>
+<? require "footer.php" ?>
--- login.php
+++ login.php
@@ -26,8 +26,4 @@
         </form>
     </center>
 </div>
-
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="js/bootstrap.js"></script>
-</body>
-</html>
+<? require "footer.php" ?>
